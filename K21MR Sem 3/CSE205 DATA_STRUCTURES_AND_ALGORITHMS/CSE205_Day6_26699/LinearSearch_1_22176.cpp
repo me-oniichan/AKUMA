c56bd48f120b4449294d3e0791285af443944151
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-08-29 21:44:53 +0530

    Redundant case reduction

--- K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day6_26699/LinearSearch_1_22176.cpp
+++ K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day6_26699/LinearSearch_1_22176.cpp
@@ -11,7 +11,7 @@ int main()
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
-            if (arr[i] == 'W' || arr[i] == 'w')
+            if (arr[i] == 'W')
             {
                 cout << i << " ";
                 count++;
