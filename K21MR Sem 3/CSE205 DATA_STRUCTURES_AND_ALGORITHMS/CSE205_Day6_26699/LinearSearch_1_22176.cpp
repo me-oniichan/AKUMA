dfee757601db410cfc61d972e24646c817e77d6a
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-08-29 21:40:09 +0530

    10/10

--- K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day6_26699/LinearSearch_1_22176.cpp
+++ K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day6_26699/LinearSearch_1_22176.cpp
@@ -1,30 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n;
+    int n, count = 0;
     cin >> n;
     char arr[n];
-    int count = 0;
-    if (n < 4 || n < 20)
+    int track[n];
+    track[0] = -1;
+    if (n <= 4 || n >= 20)
     {
-        for (int i = 0; i < n; i++)
+        cout << -1;
+        exit(0);
+    }
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        if (arr[i] == 'W')
         {
-            cin >> arr[i];
-            if (arr[i] == 'W')
-            {
-                cout << i << " ";
-                count++;
-            }
+            track[count] = i;
+            count++;
         }
-        if (count == 0)
-            cout << endl << -1;
-        else
-            cout << endl << count;
-    }
-    else
+    if (track[0] == -1)
     {
-        return 0;
+        cout << -1 << endl;
+        cout << 0;
+        exit(0);
     }
+    for (int j = 0; j < count; j++)
+        if (j == count - 1)
+            cout << track[j];
+        else
+            cout << track[j] << " ";
+    cout << endl;
+    cout << count;
     return 0;
 }
