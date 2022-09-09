75593abbb7d2fd21d34d675cdf9a704ef4274de4
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-09-09 19:08:49 +0530

    Python 9th Sept

---
+++ K21MR Sem 3/INT213 PYTHON_PROGRAMMING/K21-9Sept/String Reduction.py
@@ -0,0 +1,15 @@
+def stringReduction(s):
+    char_count = {'a':0, 'b':0, 'c':0}
+    for i in s:
+        char_count[i] += 1
+    if char_count['a']%2 == char_count['b']%2 == char_count['c']%2:
+        return 2
+    elif len(set(s)) == 1:
+        return len(s)
+    else:
+        return 1
+t = int(input())
+for i in range(t):
+    s = input()
+
+    print(stringReduction(s))
