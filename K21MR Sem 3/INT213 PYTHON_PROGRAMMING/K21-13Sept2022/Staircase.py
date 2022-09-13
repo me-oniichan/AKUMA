56a92cbe7ad85717a8831a30c5e2031ddcd581a4
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-09-13 19:34:39 +0530

    Python 13 Sept

---
+++ K21MR Sem 3/INT213 PYTHON_PROGRAMMING/K21-13Sept2022/Staircase.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+def staircase(n):
+    for stairs in range(1, n + 1):
+        print(' ' * (n - stairs) + '#' * stairs)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
