86e5f4128457567184c9c86270bc579c47e39aa7
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-09-27 22:54:15 +0530

    Python 27 Sept

---
+++ K21MR Sem 3/INT213 PYTHON_PROGRAMMING/K21-27sept.cpp/Input().py
@@ -0,0 +1,3 @@
+a = input().split()
+x = int(a[0])
+print(eval(input()) == int(a[1]))
---
+++ K21MR Sem 3/INT213 PYTHON_PROGRAMMING/K21-27sept.cpp/Zipped!.py
@@ -0,0 +1,8 @@
+n, x = map(int, input().split()) 
+
+report = []
+for _ in range(x):
+    report.append( map(float, input().split()) ) 
+
+for i in zip(*report): 
+    print(sum(i)/len(i))
