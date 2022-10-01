d9770e587ae8d5d08e43d047efc200114ef452ea
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-10-01 09:35:03 +0530

    DSA Hackerrank Q2

--- K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day25_26699/QueueImplementation2_16921.cpp
+++ K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day25_26699/QueueImplementation2_16921.cpp
@@ -1,122 +1,68 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Queue
+char myStack[10];
+int front = -1, rear = -1, count = 0;
+
+void insert(char c)
 {
-    int rear, front;
-
-    int size;
-    int *arr;
-
-public:
-    Queue(int s)
+    if (count == 7)
     {
-        front = rear = -1;
-        size = s;
-        arr = new int[s];
+        cout << "OVERFLOW" << endl;
+        exit(0);
     }
-
-    void enQueue(int value);
-    int deQueue();
-    void displayQueue();
-};
-
-void Queue::enQueue(int value)
-{
-    if ((front == 0 && rear == size - 1) ||
-        (rear == (front - 1) % (size - 1)))
+    if (rear == -1)
     {
-        printf("\nQueue is Full");
-        return;
+        front++;
+        rear++;
+        myStack[rear] = c;
+        count++;
     }
-
-    else if (front == -1)
-    {
-        front = rear = 0;
-        arr[rear] = value;
-    }
-
-    else if (rear == size - 1 && front != 0)
-    {
-        rear = 0;
-        arr[rear] = value;
-    }
-
     else
     {
         rear++;
-        arr[rear] = value;
+        myStack[rear] = c;
+        count++;
     }
 }
 
-int Queue::deQueue()
+void delete_ch()
 {
-    if (front == -1)
-    {
-        printf("\nQueue is Empty");
-        return INT_MIN;
-    }
-
-    int data = arr[front];
-    arr[front] = -1;
-    if (front == rear)
-    {
-        front = -1;
-        rear = -1;
-    }
-    else if (front == size - 1)
-        front = 0;
-    else
-        front++;
-
-    return data;
-}
-
-void Queue::displayQueue()
-{
-    if (front == -1)
-    {
-        printf("\nQueue is Empty");
-        return;
-    }
-    printf("\nElements in Circular Queue are: ");
-    if (rear >= front)
-    {
-        for (int i = front; i <= rear; i++)
-            printf("%d ", arr[i]);
-    }
-    else
-    {
-        for (int i = front; i < size; i++)
-            printf("%d ", arr[i]);
-
-        for (int i = 0; i <= rear; i++)
-            printf("%d ", arr[i]);
-    }
+    rear--;
 }
 
 int main()
 {
-    Queue q(5);
-
-    q.enQueue(14);
-    q.enQueue(22);
-    q.enQueue(13);
-    q.enQueue(-6);
-
-    q.displayQueue();
-
-    printf("\nDeleted value = %d", q.deQueue());
-    printf("\nDeleted value = %d", q.deQueue());
-
-    q.displayQueue();
-
-    q.enQueue(9);
-    q.enQueue(20);
-    q.enQueue(5);
-
-    q.displayQueue();
-
-    q.enQueue(20);
+    int key;
+    do
+    {
+        cin >> key;
+        if (key == 1)
+        {
+            char x;
+            cin >> x;
+            insert(x);
+        }
+        if (key == 2)
+        {
+            delete_ch();
+        }
+    } while (key == 1 || key == 2);
+    if (front == -1)
+    {
+        cout << "-1" << endl;
+    }
+    else
+    {
+        cout << myStack[front] << endl;
+    }
+    if (rear == -1)
+    {
+        cout << "-1" << endl;
+    }
+    else
+    {
+        cout << myStack[rear] << endl;
+    }
     return 0;
 }
